682cb675107c952eba0da7a7368596d9443c68fd
skyun <skyun@a2tec.co.kr>
2021-08-02 17:33:09 +0900

    first commit

---
+++ src/main/java/org/kenux/anything/AnythingTestApplication.java
@@ -0,0 +1,13 @@
+package org.kenux.anything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnythingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kenux/anything/AnythingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kenux.anything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnythingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
