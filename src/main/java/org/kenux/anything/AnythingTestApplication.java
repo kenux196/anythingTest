bd436b1344f72e666f3a00fbd0c37b4d3720f40d
kenux196 <kenux.yun@gmail.com>
2021-08-26 13:23:30 +0900

    component scan을 위한 basepackage class 추가

--- src/main/java/org/kenux/anything/AnythingTestApplication.java
+++ src/main/java/org/kenux/anything/AnythingTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {ComponentScanBasePackage.class})
 public class AnythingTestApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/kenux/anything/ComponentScanBasePackage.java
@@ -0,0 +1,7 @@
+package org.kenux.anything;
+
+/**
+ * 컴포넌트 스캔의 basePackage 를 설정한다.
+ */
+public class ComponentScanBasePackage {
+}
