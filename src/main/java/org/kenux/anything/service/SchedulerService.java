39df896bb2db47e7b3e7c9ab1d03c1c5c15497e1
kenux196 <kenux.yun@gmail.com>
2021-10-08 13:49:37 +0900

    Spring Boot @Scheduled Test

---
+++ src/main/java/org/kenux/anything/service/SchedulerService.java
@@ -0,0 +1,28 @@
+package org.kenux.anything.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+
+@Service
+@Slf4j
+public class SchedulerService {
+
+//    @Scheduled(initialDelay = 10000, fixedDelay = 10000)
+//    public void runAfterTenSecondsRepeatTenSeconds() {
+//        log.info("10초 후 실행 => time : " + LocalTime.now());
+//    }
+
+    @Scheduled(fixedRate = 1000)
+    public void runEveryTenSecondsOne() {
+        log.info("runEveryTenSecondsOne time : " + LocalTime.now());
+        log.info("runEveryTenSecondsOne thread: " + Thread.currentThread().getName());
+    }
+    @Scheduled(fixedRate = 1000)
+    public void runEveryTenSecondsTwo() {
+        log.info("runEveryTenSecondsTwo time : " + LocalTime.now());
+        log.info("runEveryTenSecondsTwo thread: " + Thread.currentThread().getName());
+    }
+}
